eba117926d9f03907911624b7abf564ccf3c93cc
CreedsCode <contact@creedscode.cc>
2023-11-17 08:58:09 +0100

    make sure for older node runtimes, fetch works

--- packages/inference/src/index.ts
+++ packages/inference/src/index.ts
@@ -2,3 +2,18 @@ export { HfInference, HfInferenceEndpoint } from "./HfInference";
 export { InferenceOutputError } from "./lib/InferenceOutputError";
 export * from "./types";
 export * from "./tasks";
+
+console.log(fetch, typeof fetch);
+
+if (typeof fetch !== 'undefined') {
+    throw new Error('fetch is already defined. This environment may already provide and implementation of fetch.');
+}
+
+if (typeof globalThis === 'object') {
+    globalThis.fetch = fetch;
+} else if (typeof global === 'object') {
+    global.fetch = fetch;
+} else {
+    // Everything else is not supported
+    throw new Error('Unknown JavaScript environment: Not supported');
+}
